5b9c24adcacb1f660c64803a33c8901ebad02635
luogan1234 <luogan_62625686@126.com>
2021-02-05 21:28:15 +0800

    add run.sh

---
+++ run.sh
@@ -0,0 +1,19 @@
+python init.py
+for encoder in "bilstm" "bert"; do
+  for dataset in "type_net" "med_mentions"; do
+    for task in 0; do
+      for seed in {0..1}; do
+        cmd="python main.py -dataset $dataset -text_encoder $encoder -task $task -remove_img -seed $seed"
+        echo $cmd & $cmd
+      done
+    done
+  done
+  for dataset in "flower"; do
+    for task in 0; do
+      for seed in {0..1}; do
+        cmd="python main.py -dataset $dataset -text_encoder $encoder -task $task -remove_name -seed $seed"
+        echo $cmd & $cmd
+      done
+    done
+  done
+done
